c1d6dbacaf6f523748dc349b8cd522c823e0c2ce
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-04-11 11:36:38 +0000

    - new version 3.3.4

--- LetoDMS_Core/Core/inc.ClassDMS.php
+++ LetoDMS_Core/Core/inc.ClassDMS.php
@@ -184,7 +184,7 @@ class LetoDMS_Core_DMS {
 		$this->convertFileTypes = array();
 		$this->version = '@package_version@';
 		if($this->version[0] == '@')
-			$this->version = '3.3.3';
+			$this->version = '3.3.4';
 	} /* }}} */
 
 	function getDB() { /* {{{ */

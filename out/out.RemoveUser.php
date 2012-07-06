5e9fa3cc886e7aff713d579b89c7c35603ea88e2
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-07-06 10:40:52 +0000

    - list of users contains not just login but also fullname

--- out/out.RemoveUser.php
+++ out/out.RemoveUser.php
@@ -2,7 +2,7 @@
 //    MyDMS. Document Management System
 //    Copyright (C) 2002-2005  Markus Westphal
 //    Copyright (C) 2006-2008 Malcolm Cowe
-//    Copyright (C) 2006-2008 Malcolm Cowe
+//    Copyright (C) 2010-2012 Uwe Steinmann
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -67,7 +67,7 @@ UI::contentContainerStart();
 			continue;
 
 		if (isset($_GET["userid"]) && $currUser->getID()==$_GET["userid"]) $selected=$count;
-		print "<option value=\"".$currUser->getID()."\">" . htmlspecialchars($currUser->getLogin());
+		print "<option value=\"".$currUser->getID()."\">" . htmlspecialchars($currUser->getLogin()." - ".$currUser->getFullName());
 	}
 ?>
 </select>

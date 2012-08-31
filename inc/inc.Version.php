d070930205bab1a30dc8d0a2e55e069c1aa950c5
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-08-31 07:47:58 +0000

    - new version 3.3.8

--- inc/inc.Version.php
+++ inc/inc.Version.php
@@ -19,7 +19,7 @@
 
 class LetoDMS_Version {
 
-	var $_number = "3.3.7";
+	var $_number = "3.3.8";
 	var $_string = "LetoDMS";
 
 	function LetoDMS_Version() {
--- install/install.php
+++ install/install.php
@@ -88,7 +88,7 @@ function printCheckError($resCheck) { /* {{{ */
  * Load default settings + set
  */
 define("LETODMS_INSTALL", "on");
-define("LETODMS_VERSION", "3.3.7");
+define("LETODMS_VERSION", "3.3.8");
 
 require_once('../inc/inc.ClassSettings.php');
 

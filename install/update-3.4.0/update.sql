6aac1a25a01fab7fdd60c1729fc22fc1ecbf75df
steinm <steinm@577cc39b-cfd1-4aa2-8bc3-eda205cea05a>
2012-11-27 21:02:26 +0000

    - much better update procedure

--- install/update-3.4.0/update.sql
+++ install/update-3.4.0/update.sql
@@ -1,19 +1,69 @@
-ALTER TABLE tblFolders DROP FOREIGN KEY `tblFolders_owner`;
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS DROPFK $$
+CREATE PROCEDURE DROPFK (
+IN parm_table_name VARCHAR(100),
+IN parm_key_name VARCHAR(100)
+)
+BEGIN
+IF EXISTS (SELECT NULL FROM information_schema.TABLE_CONSTRAINTS
+WHERE CONSTRAINT_SCHEMA = DATABASE() AND CONSTRAINT_NAME = parm_key_name) THEN
+ALTER TABLE parm_table_name DROP FOREIGN KEY parm_key_name;
+END IF;
+END $$
+DELIMITER ; $$
+
+ALTER TABLE tblACLs ENGINE=InnoDB;
+ALTER TABLE tblCategory ENGINE=InnoDB;
+ALTER TABLE tblUsers ENGINE=InnoDB;
+ALTER TABLE tblUserPasswordRequest ENGINE=InnoDB;
+ALTER TABLE tblUserImages ENGINE=InnoDB;
+ALTER TABLE tblFolders ENGINE=InnoDB;
+ALTER TABLE tblDocuments ENGINE=InnoDB;
+ALTER TABLE tblDocumentApprovers ENGINE=InnoDB;
+ALTER TABLE tblDocumentApproveLog ENGINE=InnoDB;
+ALTER TABLE tblDocumentContent ENGINE=InnoDB;
+ALTER TABLE tblDocumentLinks ENGINE=InnoDB;
+ALTER TABLE tblDocumentFiles ENGINE=InnoDB;
+ALTER TABLE tblDocumentLocks ENGINE=InnoDB;
+ALTER TABLE tblDocumentReviewers ENGINE=InnoDB;
+ALTER TABLE tblDocumentReviewLog ENGINE=InnoDB;
+ALTER TABLE tblDocumentStatus ENGINE=InnoDB;
+ALTER TABLE tblDocumentStatusLog ENGINE=InnoDB;
+ALTER TABLE tblGroups ENGINE=InnoDB;
+ALTER TABLE tblGroupMembers ENGINE=InnoDB;
+ALTER TABLE tblKeywordCategories ENGINE=InnoDB;
+ALTER TABLE tblKeywords ENGINE=InnoDB;
+ALTER TABLE tblDocumentCategory ENGINE=InnoDB;
+ALTER TABLE tblNotify ENGINE=InnoDB;
+ALTER TABLE tblSessions ENGINE=InnoDB;
+ALTER TABLE tblMandatoryReviewers ENGINE=InnoDB;
+ALTER TABLE tblMandatoryApprovers ENGINE=InnoDB;
+ALTER TABLE tblEvents ENGINE=InnoDB;
+ALTER TABLE tblVersion ENGINE=InnoDB;
+
+CALL DROPFK('tblFolders', 'tblFolders_owner');
+-- ALTER TABLE tblFolders DROP FOREIGN KEY `tblFolders_owner`;
 ALTER TABLE tblFolders ADD CONSTRAINT `tblFolders_owner` FOREIGN KEY (`owner`) REFERENCES `tblUsers` (`id`);
 
-ALTER TABLE tblDocuments DROP FOREIGN KEY `tblDocuments_owner`;
+CALL DROPFK('tblDocuments', 'tblDocuments_owner');
+-- ALTER TABLE tblDocuments DROP FOREIGN KEY `tblDocuments_owner`;
 ALTER TABLE tblDocuments ADD CONSTRAINT `tblDocuments_owner` FOREIGN KEY (`owner`) REFERENCES `tblUsers` (`id`);
 
-ALTER TABLE tblDocuments DROP FOREIGN KEY `tblDocuments_folder`;
+CALL DROPFK('tblDocuments', 'tblDocuments_folder');
+-- ALTER TABLE tblDocuments DROP FOREIGN KEY `tblDocuments_folder`;
 ALTER TABLE tblDocuments ADD CONSTRAINT `tblDocuments_folder` FOREIGN KEY (`folder`) REFERENCES `tblFolders` (`id`);
 
-ALTER TABLE tblDocumentContent DROP FOREIGN KEY `tblDocumentDocument_document`;
+CALL DROPFK('tblDocumentContent', 'tblDocumentDocument_document');
+-- ALTER TABLE tblDocumentContent DROP FOREIGN KEY `tblDocumentDocument_document`;
 ALTER TABLE tblDocumentContent ADD CONSTRAINT `tblDocumentContent_document` FOREIGN KEY (`document`) REFERENCES `tblDocuments` (`id`);
 
-ALTER TABLE tblDocumentLinks DROP FOREIGN KEY `tblDocumentLinks_user`;
+CALL DROPFK('tblDocumentLinks', 'tblDocumentLinks_user');
+-- ALTER TABLE tblDocumentLinks DROP FOREIGN KEY `tblDocumentLinks_user`;
 ALTER TABLE tblDocumentLinks ADD CONSTRAINT `tblDocumentLinks_user` FOREIGN KEY (`userID`) REFERENCES `tblUsers` (`id`);
 
-ALTER TABLE tblDocumentFiles DROP FOREIGN KEY `tblDocumentFiles_user`;
+CALL DROPFK('tblDocumentFiles', 'tblDocumentFiles_user');
+-- ALTER TABLE tblDocumentFiles DROP FOREIGN KEY `tblDocumentFiles_user`;
 ALTER TABLE tblDocumentFiles ADD CONSTRAINT `tblDocumentFiles_user` FOREIGN KEY (`userID`) REFERENCES `tblUsers` (`id`);
 
 ALTER TABLE tblGroupMembers DROP PRIMARY KEY;

247925cff4c23ae21ef27eeccfaad2d3db4f2b9b
Uwe Steinmann <steinm@debian.org>
2013-02-13 19:44:49 +0100

    new version 3.4.3

--- LetoDMS_Core/Core/inc.ClassDMS.php
+++ LetoDMS_Core/Core/inc.ClassDMS.php
@@ -185,7 +185,7 @@ class LetoDMS_Core_DMS {
 		$this->convertFileTypes = array();
 		$this->version = '@package_version@';
 		if($this->version[0] == '@')
-			$this->version = '3.4.2';
+			$this->version = '3.4.3';
 	} /* }}} */
 
 	function getDB() { /* {{{ */

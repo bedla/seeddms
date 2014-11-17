93510320ec515fe9407a28e8ca9d578c6c4f2252
Uwe Steinmann <steinm@debian.org>
2014-11-17 07:15:13 +0100

    new version 4.3.12

--- install/install.php
+++ install/install.php
@@ -119,7 +119,7 @@ function fileExistsInIncludePath($file) { /* {{{ */
  * Load default settings + set
  */
 define("SEEDDMS_INSTALL", "on");
-define("SEEDDMS_VERSION", "4.3.11");
+define("SEEDDMS_VERSION", "4.3.12");
 
 require_once('../inc/inc.ClassSettings.php');
 

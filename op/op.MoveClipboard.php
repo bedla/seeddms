b39b92e0b0164778b43b144a51154db55885a8de
Uwe Steinmann <steinm@debian.org>
2014-11-24 19:08:17 +0100

    don't crash if folder/document was deleted (Bug #186)

--- op/op.MoveClipboard.php
+++ op/op.MoveClipboard.php
@@ -43,25 +43,67 @@ if ($targetFolder->getAccessMode($user) < M_READWRITE) {
 $clipboard = $session->getClipboard();
 foreach($clipboard['docs'] as $documentid) {
 	$document = $dms->getDocument($documentid);
-	$oldFolder = $document->getFolder();
+	if($document) {
+		$oldFolder = $document->getFolder();
 
-	if ($document->getAccessMode($user) < M_READWRITE) {
-		UI::exitError(getMLText("document_title", array("documentname" => $document->getName())),getMLText("access_denied"));
+		if ($document->getAccessMode($user) < M_READWRITE) {
+			UI::exitError(getMLText("document_title", array("documentname" => $document->getName())),getMLText("access_denied"));
+		}
+
+		if ($targetid != $oldFolder->getID()) {
+			if ($document->setFolder($targetFolder)) {
+				// Send notification to subscribers.
+				if($notifier) {
+					$notifyList = $document->getNotifyList();
+					$subject = "document_moved_email_subject";
+					$message = "document_moved_email_body";
+					$params = array();
+					$params['name'] = $document->getName();
+					$params['old_folder_path'] = $oldFolder->getFolderPathPlain();
+					$params['new_folder_path'] = $targetFolder->getFolderPathPlain();
+					$params['username'] = $user->getFullName();
+					$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewDocument.php?documentid=".$document->getID();
+					$params['sitename'] = $settings->_siteName;
+					$params['http_root'] = $settings->_httpRoot;
+					$notifier->toList($user, $notifyList["users"], $subject, $message, $params);
+					foreach ($notifyList["groups"] as $grp) {
+						$notifier->toGroup($user, $grp, $subject, $message, $params);
+					}
+					// if user is not owner send notification to owner
+					if ($user->getID() != $document->getOwner()->getID()) 
+						$notifier->toIndividual($user, $document->getOwner(), $subject, $message, $params);
+				}
+				$session->removeFromClipboard($document);
+
+			} else {
+				UI::exitError(getMLText("document_title", array("documentname" => $document->getName())),getMLText("error_occured"));
+			}
+		} else {
+			$session->removeFromClipboard($document);
+		}
 	}
+}
 
-	if ($targetid != $oldFolder->getID()) {
-		if ($document->setFolder($targetFolder)) {
+foreach($clipboard['folders'] as $folderid) {
+	$folder = $dms->getFolder($folderid);
+	if($folder) {
+		if ($folder->getAccessMode($user) < M_READWRITE) {
+			UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("access_denied"));
+		}
+
+		$oldFolder = $folder->getParent();
+		if ($folder->setParent($targetFolder)) {
 			// Send notification to subscribers.
 			if($notifier) {
-				$notifyList = $document->getNotifyList();
-				$subject = "document_moved_email_subject";
-				$message = "document_moved_email_body";
+				$notifyList = $folder->getNotifyList();
+				$subject = "folder_moved_email_subject";
+				$message = "folder_moved_email_body";
 				$params = array();
-				$params['name'] = $document->getName();
+				$params['name'] = $folder->getName();
 				$params['old_folder_path'] = $oldFolder->getFolderPathPlain();
 				$params['new_folder_path'] = $targetFolder->getFolderPathPlain();
 				$params['username'] = $user->getFullName();
-				$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewDocument.php?documentid=".$document->getID();
+				$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewFolder.php?folderid=".$folder->getID();
 				$params['sitename'] = $settings->_siteName;
 				$params['http_root'] = $settings->_httpRoot;
 				$notifier->toList($user, $notifyList["users"], $subject, $message, $params);
@@ -69,53 +111,14 @@ foreach($clipboard['docs'] as $documentid) {
 					$notifier->toGroup($user, $grp, $subject, $message, $params);
 				}
 				// if user is not owner send notification to owner
-				if ($user->getID() != $document->getOwner()->getID()) 
-					$notifier->toIndividual($user, $document->getOwner(), $subject, $message, $params);
-			}
-			$session->removeFromClipboard($document);
+				if ($user->getID() != $folder->getOwner()->getID()) 
+					$notifier->toIndividual($user, $folder->getOwner(), $subject, $message, $params);
 
+			}
+			$session->removeFromClipboard($folder);
 		} else {
-			UI::exitError(getMLText("document_title", array("documentname" => $document->getName())),getMLText("error_occured"));
+			UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("error_occured"));
 		}
-	} else {
-		$session->removeFromClipboard($document);
-	}
-}
-
-foreach($clipboard['folders'] as $folderid) {
-	$folder = $dms->getFolder($folderid);
-
-	if ($folder->getAccessMode($user) < M_READWRITE) {
-		UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("access_denied"));
-	}
-
-	$oldFolder = $folder->getParent();
-	if ($folder->setParent($targetFolder)) {
-		// Send notification to subscribers.
-		if($notifier) {
-			$notifyList = $folder->getNotifyList();
-			$subject = "folder_moved_email_subject";
-			$message = "folder_moved_email_body";
-			$params = array();
-			$params['name'] = $folder->getName();
-			$params['old_folder_path'] = $oldFolder->getFolderPathPlain();
-			$params['new_folder_path'] = $targetFolder->getFolderPathPlain();
-			$params['username'] = $user->getFullName();
-			$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewFolder.php?folderid=".$folder->getID();
-			$params['sitename'] = $settings->_siteName;
-			$params['http_root'] = $settings->_httpRoot;
-			$notifier->toList($user, $notifyList["users"], $subject, $message, $params);
-			foreach ($notifyList["groups"] as $grp) {
-				$notifier->toGroup($user, $grp, $subject, $message, $params);
-			}
-			// if user is not owner send notification to owner
-			if ($user->getID() != $folder->getOwner()->getID()) 
-				$notifier->toIndividual($user, $folder->getOwner(), $subject, $message, $params);
-
-		}
-		$session->removeFromClipboard($folder);
-	} else {
-		UI::exitError(getMLText("folder_title", array("foldername" => $folder->getName())),getMLText("error_occured"));
 	}
 }
 

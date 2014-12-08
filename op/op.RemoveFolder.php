1744479837bf840f3b1a03e68033748ff7629e63
Uwe Steinmann <steinm@debian.org>
2014-12-08 14:46:01 +0100

    removed left overs from failed merge (Bug #190)

--- op/op.RemoveFolder.php
+++ op/op.RemoveFolder.php
@@ -20,12 +20,13 @@
 include("../inc/inc.Settings.php");
 include("../inc/inc.LogInit.php");
 include("../inc/inc.ClassEmail.php");
-include("../inc/inc.DBInit.php");
 include("../inc/inc.Language.php");
+include("../inc/inc.Init.php");
+include("../inc/inc.Extension.php");
+include("../inc/inc.DBInit.php");
 include("../inc/inc.ClassUI.php");
 include("../inc/inc.ClassController.php");
 include("../inc/inc.Authentication.php");
-include("../inc/inc.Extension.php");
 
 $tmp = explode('.', basename($_SERVER['SCRIPT_FILENAME']));
 $controller = Controller::factory($tmp[1]);
@@ -64,53 +65,19 @@ if(!$controller->run()) {
 	UI::exitError(getMLText("folder_title", array("foldername" => getMLText("invalid_folder_id"))),getMLText("invalid_folder_id"));
 }
 
-<<<<<<< HEAD
 if ($notifier) {
 	$subject = "folder_deleted_email_subject";
 	$message = "folder_deleted_email_body";
 	$params = array();
 	$params['name'] = $foldername;
-	$params['folder_path'] = $folder->getFolderPathPlain();
+	$params['folder_path'] = $parent->getFolderPathPlain();
 	$params['username'] = $user->getFullName();
 	$params['sitename'] = $settings->_siteName;
 	$params['http_root'] = $settings->_httpRoot;
+	$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewFolder.php?folderid=".$parent->getID();
 	$notifier->toList($user, $nl["users"], $subject, $message, $params);
 	foreach ($nl["groups"] as $grp) {
 		$notifier->toGroup($user, $grp, $subject, $message, $params);
-=======
-$nl =	$folder->getNotifyList();
-$foldername = $folder->getName();
-if ($folder->remove()) {
-	// Send notification to subscribers.
-	if ($notifier) {
-/*
-		$subject = "###SITENAME###: ".$folder->getName()." - ".getMLText("folder_deleted_email");
-		$message = getMLText("folder_deleted_email")."\r\n";
-		$message .= 
-			getMLText("name").": ".$folder->getName()."\r\n".
-			getMLText("folder").": ".$folder->getFolderPathPlain()."\r\n".
-			getMLText("comment").": ".$folder->getComment()."\r\n".
-			"URL: ###URL_PREFIX###out/out.ViewFolder.php?folderid=".$folder->getID()."\r\n";
-
-		$notifier->toList($user, $folder->_notifyList["users"], $subject, $message);
-		foreach ($folder->_notifyList["groups"] as $grp) {
-			$notifier->toGroup($user, $grp, $subject, $message);
-		}
-*/
-		$subject = "folder_deleted_email_subject";
-		$message = "folder_deleted_email_body";
-		$params = array();
-		$params['name'] = $foldername;
-		$params['folder_path'] = $parent->getFolderPathPlain();
-		$params['username'] = $user->getFullName();
-		$params['sitename'] = $settings->_siteName;
-		$params['http_root'] = $settings->_httpRoot;
-		$params['url'] = "http".((isset($_SERVER['HTTPS']) && (strcmp($_SERVER['HTTPS'],'off')!=0)) ? "s" : "")."://".$_SERVER['HTTP_HOST'].$settings->_httpRoot."out/out.ViewFolder.php?folderid=".$parent->getID();
-		$notifier->toList($user, $nl["users"], $subject, $message, $params);
-		foreach ($nl["groups"] as $grp) {
-			$notifier->toGroup($user, $grp, $subject, $message, $params);
-		}
->>>>>>> seeddms-4.3.4
 	}
 }
 

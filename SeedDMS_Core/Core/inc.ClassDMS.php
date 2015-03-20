358c239c3ac64985fde3a314834d0dfc95a05686
Uwe Steinmann <steinm@debian.org>
2015-03-20 08:16:05 +0100

    new version 4.3.16

--- SeedDMS_Core/Core/inc.ClassDMS.php
+++ SeedDMS_Core/Core/inc.ClassDMS.php
@@ -262,7 +262,7 @@ class SeedDMS_Core_DMS {
 		$this->convertFileTypes = array();
 		$this->version = '@package_version@';
 		if($this->version[0] == '@')
-			$this->version = '4.3.14';
+			$this->version = '4.3.16';
 	} /* }}} */
 
 	/**

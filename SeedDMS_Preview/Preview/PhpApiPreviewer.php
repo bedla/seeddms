f0f19177274eb669604b5e5a7c0c2abb5e97cec3
Ivo Smid <ivo.smid@gmail.com>
2015-05-10 15:35:46 +0200

    Use Imagick API to create file preview

--- SeedDMS_Preview/Preview/PhpApiPreviewer.php
+++ SeedDMS_Preview/Preview/PhpApiPreviewer.php
@@ -3,26 +3,32 @@
 class SeedDMS_Preview_PhpApiPreviewer extends SeedDMS_Preview_Previewer {
 
     protected function internalCreatePreview($mimeType, $width, $file, $target) { /* {{{ */
-        // TODO implement
-//        switch($mimeType) {
-//            case "image/png":
-//            case "image/gif":
-//            case "image/jpeg":
-//            case "image/jpg":
-//            case "image/svg+xml":
-//                $cmd = 'convert -resize '.$width.'x '.$file.' '.$target;
-//                break;
-//            case "application/pdf":
-//            case "application/postscript":
-//                $cmd = 'convert -density 100 -resize '.$width.'x '.$file.'[0] '.$target;
-//                break;
-//            case "text/plain":
-//                $cmd = 'convert -resize '.$width.'x '.$file.'[0] '.$target;
-//                break;
-//            case "application/x-compressed-tar":
-//                $cmd = 'tar tzvf '.$file.' | convert -density 100 -resize '.$width.'x text:-[0] '.$target;
-//                break;
-//        }
+        switch($mimeType) {
+            case "image/png":
+            case "image/gif":
+            case "image/jpeg":
+            case "image/jpg":
+            case "image/svg+xml":
+                $img = new Imagick($file);
+                $img->scaleImage($width, 0);
+                $img->writeImage($target);
+                $img->destroy();
+            break;
+            case "application/pdf":
+            case "application/postscript":
+                $img = new Imagick($file.'[0]');
+                $img->setResolution(100, 100);
+                $img->scaleImage($width, 0);
+                $img->writeImage($target);
+                $img->destroy();
+                break;
+            case "text/plain":
+                $img = new Imagick($file.'[0]');
+                $img->scaleImage($width, 0);
+                $img->writeImage($target);
+                $img->destroy();
+                break;
+        }
     } /* }}} */
 
 }
